ffbcfb95dc3277a5c922f0eb83b52a571b0bdd76
Raniere Silva <Raniere.CostadaSilva@gesis.org>
2024-07-25 17:31:15 +0200

    Add logger

--- src/dummy/views.py
+++ src/dummy/views.py
@@ -1,8 +1,14 @@
-from flask import Blueprint
+from flask import Blueprint, current_app
 
 index_page = Blueprint("index_page", __name__)
 
 
 @index_page.route("/")
 def index():
+    current_app.logger.debug("A debug message")
+    current_app.logger.info("An info message")
+    current_app.logger.warning("A warning message")
+    current_app.logger.error("An error message")
+    current_app.logger.critical("A critical message")
+
     return {"status": "OK"}
